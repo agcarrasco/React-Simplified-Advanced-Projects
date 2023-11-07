db7fb1f6a8ff70cd2746eeee0f25772c28e5a3d6
Angic Carrasco <100764990+agcarrasco@users.noreply.github.com>
2023-11-07 22:01:53 +0800

    feat: create DialogModal component

--- 04-05-modal/before/src/App.jsx
+++ 04-05-modal/before/src/App.jsx
@@ -1,20 +1,39 @@
 import { useState } from "react";
 import { CustomModal } from "./components/CustomModal";
+import { DialogModal } from "../../after/src/DialogModal";
 
 function App() {
   const [showCustomModal, setShowCustomModal] = useState(false);
+  const [showDialogModal, setShowDialogModal] = useState(false);
 
   return (
     <>
-      <button onClick={() => setShowCustomModal(true)}>Show Custom Modal</button>
+      <button onClick={() => setShowCustomModal(true)}>
+        Show Custom Modal
+      </button>
       <br />
-      <button>Show Dialog Modal</button>
-      <CustomModal isOpen={showCustomModal} onClose={() => setShowCustomModal(false)}>
+      <button onClick={() => setShowDialogModal(true)}>
+        Show Dialog Modal
+      </button>
+      <CustomModal
+        isOpen={showCustomModal}
+        onClose={() => setShowCustomModal(false)}
+      >
         <p>
           This is a <strong>CUSTOM</strong> modal
         </p>
         <button onClick={() => setShowCustomModal(false)}>Close</button>
       </CustomModal>
+
+      <DialogModal
+        isOpen={showDialogModal}
+        onClose={() => setShowDialogModal(false)}
+      >
+        <p>
+          This is a <strong>DIALOG</strong> modal
+        </p>
+        <button onClick={() => setShowDialogModal(false)}>Close</button>
+      </DialogModal>
     </>
   );
 }
